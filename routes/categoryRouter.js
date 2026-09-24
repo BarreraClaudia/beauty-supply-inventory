c878675719dc890ef841dec05fce4a9b0b875552
BarreraClaudia <claudiabarrera.code@gmail.com>
2026-09-24 14:14:15 -0600

    add: validate id param

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -8,6 +8,14 @@ categoryRouter.get('/', categoryController.categoryList);
 categoryRouter.get('/create', categoryController.categoryCreateGet);
 categoryRouter.post('/create', categoryController.categoryCreatePost);
 
+categoryRouter.param('id', (req, res, next, id) => {
+  // checks if req param is NOT a positive integer
+  if (!/^\d+$/.test(id)) {
+    return res.status(404).render('404');
+  }
+  next();
+});
+
 categoryRouter.get('/:id', categoryController.categoryProducts);
 
 categoryRouter.get('/:id/update', categoryController.categoryUpdateGet);
